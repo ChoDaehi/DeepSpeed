0055710263e915b6980e699b9f6ce7b6155ccc88
daehi <chodaehi03@gmaiil.com>
2025-04-23 14:52:53 +0900

    ノード数とモデル名をコマンドライン変数に設定

--- trainer.py
+++ trainer.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader, Dataset
 import torch.distributed as dist
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, AdamW, AutoModelForCausalLM, AutoTokenizer,BitsAndBytesConfig
 import deepspeed
+import argparse
 
 class SimpleDataset(Dataset):
     def __init__(self, tokenizer, texts):
@@ -13,8 +14,21 @@ class SimpleDataset(Dataset):
         input_id = self.input_ids[idx]
         return {'input_ids': input_id, 'labels': input_id}
 
-#tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.3-70B-Instruct", use_fast=False,device_map="auto")
+# コマンドライン引数を定義
+parser = argparse.ArgumentParser(description="DeepSpeed training script")
+# DeepSpeed用に引数を解析
+parser = deepspeed.add_config_arguments(parser)  # DeepSpeed関連の引数を追加
+parser.add_argument("--mp_size", type=int, default=1, help="Model parallelism size")
+parser.add_argument("--local_rank", type=int, default=0, help="Model parallelism size")
+parser.add_argument("--model_name", type=str, default="gpt2", help="DeepSpeed config file path")
+# 引数を解析
+args = parser.parse_args()
+
+print(f"Local rank: {args.local_rank}")
+print(f"Model parallelism size: {args.mp_size}")
+
+model_name = args.model_name
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 texts = ["Hello, DeepSpeed!", "DeepSpeed makes large model training efficient."]
 dataset = SimpleDataset(tokenizer, texts)
@@ -27,11 +41,22 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_quant_type="nf4",
     bnb_4bit_compute_dtype=torch.float16
 )
+model = GPT2LMHeadModel.from_pretrained('gpt2')
 
-#model = GPT2LMHeadModel.from_pretrained('gpt2')
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.3-70B-Instruct",device_map="auto")
-model = deepspeed.init_inference(model, mp_size=1, dtype=torch.float16)
-
+model = AutoModelForCausalLM.from_pretrained(
+            pretrained_model_name_or_path=model_name,
+                device_map="auto",  # 여러 GPU에 자동 분산
+                    torch_dtype=torch.float16,
+                        low_cpu_mem_usage=True
+                        )
+print('pretrained model load ok!')
+model = deepspeed.init_inference(
+                model,
+                mp_size=args.mp_size,  # 사용 가능한 GPU 수에 맞게 조정
+                dtype=torch.float16,
+                replace_method="auto",
+                replace_with_kernel_inject=False,
+            )
 
 # Initialize DeepSpeed
 model, optimizer, _, lr_scheduler = deepspeed.initialize(
@@ -50,8 +75,6 @@ for epoch in range(3):
         model.step()
 
         print(f'Epoch: {epoch}, Loss: {loss.item()}')
-
-
 
 # トレーニング処理（DeepSpeedなど）が終了した後に追加
 if dist.is_initialized():  # プロセスグループが初期化されている場合のみ実行
